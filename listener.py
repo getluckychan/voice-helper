4b0c68754337c44d97376ce978c676cd90473346
ivan <lucky.boy.fot@gmail.com>
2022-01-21 15:25:30 +0200

    123

--- listener.py
+++ listener.py
@@ -1,21 +1,41 @@
+from enum import Enum
+from abc import ABC, abstractmethod
 
 
-def voice_checker():
-    voice = 'работаем'
-    from os import system
-    if voice == 'открой хром':
-        system("google-chrome")
-    elif voice == 'открой телеграм':
-        system("telegram-desktop")
-    elif voice == 'открой спотифай':
-        system("spotify")
-    elif voice == 'заглуши':
-        system("amixer -D pulse sset Master 0%")
-    elif voice == 'включи звук':
-        system("amixer -D pulse sset Master 50%")
-    else:
-        print('something went wrong')
+class Command(ABC):
+    @abstractmethod
+    def check(self, text):
+        pass
 
 
-voice_checker()
+class OpenCommand(Command):
+    open = 'open'
 
+    class SysCommand(Enum):
+        chrome = "google-chrome"
+        telegram = "telegram-desktop"
+        spotify = "spotify"
+
+    def check(self, text):
+        text = text.lower()
+        for command in self.SysCommand:
+            if command.name in text:
+                from os import system
+                print("opening: " + command.name)
+                system(command.value)
+
+
+class SoundCommand(Command):
+    sound = "sound"
+
+    class SysCommand(Enum):
+        mute = "amixer -c 0 set Mic mute"
+        unmute = "amixer -c 0 set Mic unmute"
+
+    def check(self, text):
+        text = text.lower()
+        for command in self.SysCommand:
+            if command.name in text:
+                from os import system
+                print("working: " + command.name)
+                system(command.value)
--- play.py
+++ play.py
@@ -1,55 +1,21 @@
 import speech_recognition as sr
-from enum import Enum
+from listener import *
 
 
 def on_listen(recognizer, audio):
     # print('on_listen')
     try:
-        voice = recognizer.recognize_google(audio, language='ru-RU').lower()
+        voice = recognizer.recognize_google(audio, language='en-US').lower()
     except sr.UnknownValueError as e:
         pass
     else:
-        command = OpenableCommand(voice)
-        command.check(voice)
-        # print('Вы сказали:', voice)
-        # from os import system
-        # if 'открой Chrome' or 'Chrome' in voice:
-        #     system("google-chrome")
-        # elif 'открой telegram' or 'telegram' in voice:
-        #     system("telegram-desktop")
-        # elif 'открой спотифай' or 'spotify' in voice:
-        #     system("spotify")
-        # elif 'заглуши' or 'выключи звук' in voice:
-        #     system("amixer -D pulse sset Master 0%")
-        # elif 'включи звук' or 'запусти' in voice:
-        #     system("amixer -D pulse sset Master 50%")
-        # else:
-        #     print('something went wrong')
-
-
-class Command:
-    def __init__(self, text):
-        self.text = text
-
-    def check(self, text):
-        pass
-
-
-class OpenableCommand(Command):
-    open = 'Открыть'
-
-    class SysCommand(Enum):
-        chrome = "google-chrome"
-        telegram = "telegram-desktop"
-        spotify = "spotify"
-
-    def check(self, text):
-        text = text.lower()
-        for command in self.SysCommand:
-            if command.name in text:
-                from os import system
-                print("Открываю: " + command.name)
-                system(command.value)
+        print(voice)
+        if "open" in voice:
+            command = OpenCommand()
+            command.check(voice)
+        elif "sound" in voice:
+            command = SoundCommand()
+            command.check(voice)
 
 
 r = sr.Recognizer()
