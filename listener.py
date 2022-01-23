2d7b8d9231ff1e76a4c8d7774bcfc5bba39b3dee
ivan <lucky.boy.fot@gmail.com>
2022-01-24 00:44:12 +0200

    123

--- listener.py
+++ listener.py
@@ -1,87 +1,76 @@
 from enum import Enum
-from abc import ABC, abstractmethod
 from os import system
 
 
-class Command(ABC):
-    def __init__(self, text):
-        try:
-            self.text = text.value
-        except IndexError:
-            self.text = "ne priletelo, a priletit po ebaly za takoe"
+class OpenCommand(Enum):
+    chrome = "google-chrome"
+    telegram = "telegram-desktop"
+    spotify = "spotify"
+
+
+def check_open(string):
+    text = string.value
+    for cmd in text.split(" "):
+        checking = ["open"
+                    ]
+        if cmd in checking:
+            for command in OpenCommand:
+                if command.name in text:
+                    print("opening: " + command.name)
+                    system(command.value)
+
+
+class SoundCommand(Enum):
+    off = "amixer set Master mute"
+    on = "amixer set Master unmute\n" \
+         "amixer set Headphone unmute\n" \
+         "amixer set Speaker unmute"
+    increase = "amixer -D pulse sset Master 20%+"
+    decrease = "amixer -D pulse sset Master 20%-"
+
+
+def check_sound(string):
+    text = string.value
+    for cmd in text.split(" "):
+        checking = ["volume",
+                    "sound"
+                    ]
+        if cmd in checking:
+            for command in SoundCommand:
+                if command.name in text:
+                    print("working: " + command.name)
+                    system(command.value)
+
+
+command_need_to_confirm = []
+
+
+class SysCommand(Enum):
+    shutdown = "poweroff"
+    reboot = "reboot"
+
+
+def check_sys(string):
+    text = string.value
+    com = text.split()
+    com = ''.join(com)
+    for command in SysCommand:
+        if command.name in com:
+            command_need_to_confirm.append(command.value)
             pass
 
-    @abstractmethod
-    def check(self):
+
+def check_bool(text):
+    try:
+        checking = {
+            "yes": f'{command_need_to_confirm[0]}',
+            "no": "dont say what you dont want"
+        }
+        print("are u sure")
+        try:
+            system(checking.get(text))
+            print("don`t say what you want")
+        except TypeError:
+            pass
+    except IndexError:
         pass
-
-
-class OpenCommand(Command):
-    class SysCommand(Enum):
-        chrome = "google-chrome"
-        telegram = "telegram-desktop"
-        spotify = "spotify"
-
-    def check(self):
-        for cmd in self.text.split(" "):
-            checking = ["open"
-                        ]
-            if cmd in checking:
-                for command in self.SysCommand:
-                    if command.name in self.text:
-                        print("opening: " + command.name)
-                        system(command.value)
-
-
-class SoundCommand(Command):
-    class SysCommand(Enum):
-        off = "amixer set Master mute"
-        on = "amixer set Master unmute\n" \
-             "amixer set Headphone unmute\n" \
-             "amixer set Speaker unmute"
-        increase = "amixer -D pulse sset Master 20%+"
-        decrease = "amixer -D pulse sset Master 20%-"
-
-    def check(self):
-        for cmd in self.text.split(" "):
-            checking = ["volume",
-                        "sound"
-                        ]
-            if cmd in checking:
-                for command in self.SysCommand:
-                    if command.name in self.text:
-                        print("working: " + command.name)
-                        system(command.value)
-
-
-class SystemCommand(Command):
-    def __init__(self):
-        super().__init__(self.text)
-        self.command_need_to_confirm = []
-
-    class SysCommand(Enum):
-        shutdown = "poweroff"
-        reboot = "reboot"
-
-    def check(self):
-        com = self.text.split()
-        com = ''.join(com)
-        for command in self.SysCommand:
-            if command.name in com:
-                self.command_need_to_confirm.append(command.value)
-                pass
-
-    def check_bool(self):
-        try:
-            checking = {
-                "yes": f'{self.command_need_to_confirm[0]}',
-                "no": "dont say what you dont want"
-            }
-            print("are u sure")
-            try:
-                system(checking.get(self.text))
-                print("don`t say what you want")
-            except TypeError:
-                pass
-        except IndexError:
-            pass
--- threads_processes.py
+++ threads_processes.py
@@ -4,37 +4,28 @@ from listener import *
 from ctypes import c_char_p
 
 
-# def list_to_string(s):
-#     str1 = ""
-#     return str1.join(s)
-#
-#
-# def f(a):
-#     for i in range(len(a)):
-#         a[i] = a[i]
-#
-#
-# def recognize_process():
-#     arr = Array('i', [ord(c) for c in converting()])
-#     p = Process(target=f, args=(arr,))
-#     p.start()
-#     p.join()
-#     voice = list_to_string([chr(c) for c in arr[:]])
-#     return voice
-#
-#
-# def opening_process():
-#     OpenCommand(recognize_process()).check()
-#
-#
-# if __name__ == '__main__':
-#     opening_process()
+list_of_processes = [
+
+]
+
+
+def processes():
+    process_open = Process(target=check_open, args=(string,))
+    process_sound = Process(target=check_sound, args=(string,))
+    process_sys = Process(target=check_sys, args=(string,))
+    process_open.start()
+    process_sound.start()
+    process_sys.start()
+    list_of_processes.append(process_open)
+    list_of_processes.append(process_sound)
+    list_of_processes.append(process_sys)
 
 
 if __name__ == '__main__':
-    manager = Manager()
-    string = manager.Value(c_char_p, converting())
-    process = Process(target=OpenCommand.check, args=(string,))
-    process.start()
-    process.join()
-    print(string.value)
+    while True:
+        manager = Manager()
+        string = manager.Value(c_char_p, converting())
+        processes()
+        for process in list_of_processes:
+            process.join()
+
